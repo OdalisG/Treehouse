8835d7111c8e81f76e75b58a19abc605742ec6c5
Chica502 <oxg101@aol.com>
2018-01-01 16:23:32 +0000

    Treehouse

---
+++ Treehouse/lumberjack.py
@@ -0,0 +1,6 @@
+name = input("What's your name? ")
+
+if name == "Alyson": 
+    print("{} is a rockstar and she's OK!".format(name))
+else:
+    print("{} sleeps all night and {} works all day!".format(name, name))
